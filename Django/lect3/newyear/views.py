2c44fae49ecd90ab6c75ed85359e9174f926cfe2
avinash <chunduriavinash6@gmail.com>
2021-02-14 22:44:13 +0530

    Newyear app and Django text

--- Django/lect3/newyear/views.py
+++ Django/lect3/newyear/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 import datetime
 from django.http import HttpResponse
 from time import gmtime, strftime
+from django import forms
 
 # Create your views here.
+class Contact(forms.Form):
+    yourname=forms.CharField()
+
 def index(request):
     now=datetime.datetime.now()
     return render(request,'newyear/index.html',{
