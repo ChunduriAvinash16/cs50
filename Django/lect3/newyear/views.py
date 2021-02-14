2dea0ab7f183eb45591fa3ac5433b94a562cc211
avinash <chunduriavinash6@gmail.com>
2021-02-14 22:44:01 +0530

    Newyear app and Django text

--- Django/lect3/newyear/views.py
+++ Django/lect3/newyear/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 import datetime
 from django.http import HttpResponse
+from time import gmtime, strftime
+
 # Create your views here.
 def index(request):
     now=datetime.datetime.now()
     return render(request,'newyear/index.html',{
-        "newyear":now.month==1 and now.day==1
+        "newyear":now.month==1 and now.day==1,
+        "time":strftime("%A, %d %B %Y %H:%M:%S %Z", gmtime())
     })
